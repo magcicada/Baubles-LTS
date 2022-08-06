d0c4b6239203b1214784b9410e4b5c71d4cb0c4b
X_Niter <drumsforlife1999@yahoo.com>
2022-08-06 11:40:14 -0500

    Added API methods, removed Deprecation annotation

--- src/main/java/baubles/api/BaublesApi.java
+++ src/main/java/baubles/api/BaublesApi.java
@@ -41,9 +41,24 @@ public class BaublesApi {
 	}
 
 	/**
+	 * @param livingEntity  The ItemStack to get the bauble inventory capability from
+	 * @return  Optional of the bauble inventory capability attached to the entity
+	 */
+	public static Optional<IBaublesItemHandler> getOBaublesHandler(@Nonnull final EntityLivingBase livingEntity) {
+		return Optional.ofNullable(livingEntity.getCapability(BaublesCapabilities.CAPABILITY_BAUBLES, null));
+	}
+
+	/**
+	 * @param stack The ItemStack to get the bauble capability from
+	 * @return  Optional of the bauble capability attached to the ItemStack
+	 */
+	public static Optional<IBauble> getOBaubles(ItemStack stack) {
+		return Optional.ofNullable(stack.getCapability(BaublesCapabilities.CAPABILITY_ITEM_BAUBLE, null));
+	}
+
+	/**
 	 * Retrieves the baubles capability handler wrapped as a IInventory for the supplied player
 	 */
-	@Deprecated
 	public static IInventory getBaubles(EntityPlayer player)
 	{
 		IBaublesItemHandler handler = player.getCapability(BaublesCapabilities.CAPABILITY_BAUBLES, null);
@@ -61,6 +76,22 @@ public class BaublesApi {
 			if (!handler.getStackInSlot(a).isEmpty() && handler.getStackInSlot(a).getItem()==bauble) return a;
 		}
 		return -1;
+	}
+
+	/**
+	 * Registers a resource location to be used as the slot overlay icon in the GUI
+	 * @param id                The identifier of the type of bauble to be associated with the icon
+	 * @param resourceLocation  The resource location of the icon
+	 */
+	public static void registerIcon(String id, @Nonnull ResourceLocation resourceLocation) {
+		BaubleType.iconQueues.computeIfAbsent(id, k -> new ConcurrentSet<>()).add(resourceLocation);
+	}
+
+	/**
+	 * @return  A map of identifiers and their registered icons
+	 */
+	public static Map<String, ResourceLocation> getIcons() {
+		return ImmutableMap.copyOf(BaubleType.icons);
 	}
 
 }
